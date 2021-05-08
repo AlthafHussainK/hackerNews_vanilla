36d85c01b3101235ef89808e83b900b1e296f3c8
AlthafHussainK <althafakm@gmail.com>
2021-05-09 01:48:51 +0530

    format stories

--- components/Story.js
+++ components/Story.js
@@ -1,4 +1,28 @@
 export default function Story(story) {
-  console.log(story)
-  return `<div>story</div>`
+  //console.log(story)
+  return `
+    <div class="story">
+      <div>
+        <span class="gray">${story.index}</span>
+        <span class="upvote">▲</span>
+        <a href="${story.url}">${story.title}</a>
+        <span>(${story.domain})</span>
+      </div>
+      <div>
+        <div class="gray">
+          ${story.points} points by ${story.user} ${story.time_ago}
+          |
+          <a href="#item?id=${story.id}">
+            ${story.comments_count} comments
+          </a>
+          |
+          <span class="favorite">
+            ♥︎
+            Add to Favorites
+          </span>
+          
+        </div>
+      </div>
+    </div>
+  `;
 }
