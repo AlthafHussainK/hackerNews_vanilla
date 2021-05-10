1bf83a62f4f4724331a4673984f56d985363e56b
AlthafHussainK <althafakm@gmail.com>
2021-05-10 11:01:41 +0530

    Import item

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 import Stories from './pages/stories.js';
+import Item from './pages/item.js'
 
 const router = new Navigo(null, true, '#');
 
@@ -12,7 +13,8 @@ export default class RouterHandler {
       { path: '/', page: Stories },
       { path: '/new', page: Stories },
       { path: '/ask', page: Stories },
-      { path: '/show', page: Stories }
+      { path: '/show', page: Stories },
+      { path: '/item', page: Item }
     ];
 
     routes.forEach(({ path, page}) => {
